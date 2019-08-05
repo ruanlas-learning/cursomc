175552423869eae0393f36d0a293002a7addf89f
Ruan Silva <ruanluiz.as@gmail.com>
2019-08-04 21:59:56 -0300

    Bonus: ajustando o tamanho da imagem

--- src/main/java/com/example/cursomc/services/ClienteService.java
+++ src/main/java/com/example/cursomc/services/ClienteService.java
@@ -56,6 +56,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	public Cliente find(Integer id) {
 		UserSS user = UserServices.authenticated();
 		if ( user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId()) ) {
@@ -135,6 +138,9 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return fileStorageService.storeFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
